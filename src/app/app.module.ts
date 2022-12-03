95168d56663064501b2072b8f0dc5ca18480c304
cesarauguto316 <cesaraugusto316@protonmail.com>
2022-12-03 10:48:47 -0500

    ✅ @Input &  @Output

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,26 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { MyListComponent } from './my-list/my-list.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    MyListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: FormComponent },
+      { path: 'home', component: FormComponent },
+      { path: 'mylist', component: MyListComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
